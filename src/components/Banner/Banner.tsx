21a73774ff458b7148ee46028d2dd855113f382b
Nikita Pashinsky <nikitapashinsky@gmail.com>
2025-05-22 10:19:15 +0200

    Initial commit

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+export interface BannerProps {
+  title: string;
+  subtitle?: string;
+}
+
+export default function Banner({ title, subtitle }: BannerProps) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {subtitle && <h2>{subtitle}</h2>}
+    </div>
+  );
+}
